71724aeb45dd47dbe60ef3cb436a483bfe63c8e5
Darius Quick <quickdarius@yahoo.com>
2025-06-23 13:12:23 -0400

    Module 6 complete baseline

---
+++ travlr/app_api/routes/index.cjs
@@ -0,0 +1,20 @@
+const express = require('express'); // Express app
+const router = express.Router(); // Router logic
+
+// This is where we import the controllers we will route
+const tripsController = require('../controllers/trips.cjs');
+
+// Define routes for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+
+// GET Method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
+
+
+module.exports = router;
